9ff6beaca7b78044408fe5d3d10c7fab5a3d4ac4
domalee <136012609+domalee@users.noreply.github.com>
2023-07-25 16:13:10 -0700

    Add files via upload

---
+++ app1/server.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../')
+import random
+from libs.base import adder2
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
+
+def hello_world(request):
+    x, y = random.randint(1, 100), random.randint(1, 100)
+    res = adder2.add2(x, y)
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\nThe sum is " + res
+    return Response(message)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
